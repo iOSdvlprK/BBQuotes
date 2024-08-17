bd9b0e2c229331a82e33313c646d81b238051565
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2024-08-17 21:48:33 +0900

    urls and models

---
+++ BBQuotes/Character.swift
@@ -0,0 +1,18 @@
+//
+//  Character.swift
+//  BBQuotes
+//
+//  Created by joe on 8/17/24.
+//
+
+import Foundation
+
+struct Character: Decodable {
+    let name: String
+    let birthday: String
+    let occupations: [String]
+    let images: [URL]
+    let aliases: [String]
+    let status: String
+    let portrayedBy: String
+}
---
+++ BBQuotes/Death.swift
@@ -0,0 +1,15 @@
+//
+//  Death.swift
+//  BBQuotes
+//
+//  Created by joe on 8/17/24.
+//
+
+import Foundation
+
+struct Death: Decodable {
+    let character: String
+    let image: URL
+    let details: String
+    let lastWords: String
+}
